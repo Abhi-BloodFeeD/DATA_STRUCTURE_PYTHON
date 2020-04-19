49f33f221d57afd0af5671e6ea72f92fefa33a2c
unknown <abhinavmetta@gmail.com>
2020-04-19 20:30:25 +0530

    plaindrome finder

--- linked_list/linklist.py
+++ linked_list/linklist.py
@@ -199,19 +199,4 @@ class LinkedList():
 
 
 
-"""
-llist = LinkedList()
-llist.append('1')
-llist.append('2')
-llist.append('3')
-llist.append('4')
-llist.append('5')
-llist.append('6')
-
-llist.list_rotate("4")
-llist.print_list()
-"""
-
-
-
 
---
+++ linked_list/palindrome.py
@@ -0,0 +1,17 @@
+from linklist import LinkedList
+
+def palindrome(linked_list):
+    cur = linked_list.head
+    sur = linked_list.head
+
+    s=[]
+    isTrue = True
+    while sur:
+        s.append(sur.data)   
+        sur=sur.next
+    for elements in s[::-1]:
+        if elements == cur.data:
+            cur=cur.next
+        else:
+            return False    
+    return True
